b36d1e28e43163566cdcdf5f9bacb9b5b241e270
PikachuHy <pikachuhy@163.com>
2023-04-16 16:39:46 +0800

    add packages

--- init.el
+++ init.el
@@ -35,7 +35,6 @@
         (add-subdirs-to-load-path subdir-path)))))
 
 (add-subdirs-to-load-path "~/.emacs.d/lisp")
-(add-to-list 'load-path "~/.emacs.d/lisp/vertico")
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -44,10 +43,11 @@
  ;'(package-selected-packages
  ;  '(wgrep orderless magit embark-consult embark marginalia consult vertico popon yasnippet markdown-mode posframe))
  '(tool-bar-mode nil))
+(require 'orderless)
+(require 'consult)
 (require 'vertico)
 (require 'marginalia)
 (require 'embark)
-(require 'consult)
 (require 'yasnippet)
 (require 'posframe)
 
@@ -72,19 +72,6 @@
 		       :extend nil :overline nil :underline nil :slant normal :weight normal
 		       :height 180 :width normal :foundry "nil"
 		       :family "Menlo")))))
-
-(add-to-list 'load-path "~/.emacs.d/lisp/lsp-bridge")
-(add-to-list 'load-path "~/.emacs.d/lisp/acm-terminal")
-(add-to-list 'load-path "~/.emacs.d/lisp/blink-search")
-(add-to-list 'load-path "~/.emacs.d/lisp/sort-tab")
-(add-to-list 'load-path "~/.emacs.d/lisp/color-rg")
-(add-to-list 'load-path "~/.emacs.d/lisp/exec-path-from-shell")
-(add-to-list 'load-path "~/.emacs.d/lisp/duplicate-line")
-(add-to-list 'load-path "~/.emacs.d/lisp/awesome-tab")
-(add-to-list 'load-path "~/.emacs.d/lisp/awesome-tray")
-(add-to-list 'load-path "~/.emacs.d/lisp/all-the-icons.el")
-
-
 
 (require 'yasnippet)
 (yas-global-mode 1)
@@ -116,6 +103,7 @@
 (global-set-key (kbd "C-s") 'consult-line)
 (global-set-key (kbd "s-f") 'consult-line)
 
-(add-to-list 'load-path "~/.emacs.d/lisp/projectile")
 (require 'projectile)
 (projectile-mode 1)
+
+(require 'bazel)
